2f9737f8e3ca537afec2d3e1183e81414afbfe3e
kaleoleonhardt <kaleoleonhardt@gmail.com>
2019-01-06 21:38:44 -1000

    Complete

--- week2/ch08/array_sort.rb
+++ week2/ch08/array_sort.rb
@@ -3,8 +3,8 @@ word_list = []
 puts 'Please enter the desired words (one per line), and press enter when finished:'
 
 # Fetch the words, then output the sorted list
-while ()
-
+while (input = gets.chomp) != ""
+  word_list.push input.downcase
 end
-
-puts word_list # Make sure its sorted!
+word_list_sorted = word_list.sort
+puts word_list_sorted.join(", ") # Make sure its sorted!
--- week2/ch08/table_of_contents.rb
+++ week2/ch08/table_of_contents.rb
@@ -1,9 +1,16 @@
 # Make sure you read the whole tip section, there's some good stuff there!
 
-line_width = # Set it to a default length you'll use throughout
+line_width = 80
 title = "Table of Contents"
 
 # Depending on your solution, you may use more than one array, but here's one to get you started
-chapters = [] 
-
+chapters = [["Getting Started", "Page 1 "], ["Numbers", "Page 9 "], ["Letters", "Page 13"], ["Variables and Assignment", "Page 17"], ["Mixing It Up", "Page 21"], ["More About Methods", "Page 27"], ["Flow Control", "Page 37"], ["Arrays and Iterators", "Page 51"], ["Writing Your Own Methods", "Page 57"]]
 ### Your Code Here ###
+puts title.center(line_width)
+chap_num = 0
+chapters.each do |chap|
+  chap_title = chap[0]
+  page = chap[1]
+  chap_num = chap_num +1
+  puts ("Chapter " + chap_num.to_s + ": " + chap_title).ljust(line_width/2) + page.rjust(line_width/2)
+end
